203aea0c902bec5223b4c8cf7dd422fa8a4801ef
Peter Koszorús <p.koszorus27@gmail.com>
2024-05-26 13:22:31 +0200

    Added openapi

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,12 +7,18 @@
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
     interface PkoAmbulanceWlApp {
+        "ambulanceId": string;
+        "apiBase": string;
         "basePath": string;
     }
     interface PkoAmbulanceWlEditor {
+        "ambulanceId": string;
+        "apiBase": string;
         "entryId": string;
     }
     interface PkoAmbulanceWlList {
+        "ambulanceId": string;
+        "apiBase": string;
     }
 }
 export interface PkoAmbulanceWlEditorCustomEvent<T> extends CustomEvent<T> {
@@ -72,13 +78,19 @@ declare global {
 }
 declare namespace LocalJSX {
     interface PkoAmbulanceWlApp {
+        "ambulanceId"?: string;
+        "apiBase"?: string;
         "basePath"?: string;
     }
     interface PkoAmbulanceWlEditor {
+        "ambulanceId"?: string;
+        "apiBase"?: string;
         "entryId"?: string;
         "onEditor-closed"?: (event: PkoAmbulanceWlEditorCustomEvent<string>) => void;
     }
     interface PkoAmbulanceWlList {
+        "ambulanceId"?: string;
+        "apiBase"?: string;
         "onEntry-clicked"?: (event: PkoAmbulanceWlListCustomEvent<string>) => void;
     }
     interface IntrinsicElements {
